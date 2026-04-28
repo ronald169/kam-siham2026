30d27b2b22153fba26b5cb1a9cce6dbfe17b6fce
mellTrust <ronalsgatsi@gmail.com>
2026-04-28 15:09:10 +0100

    initialisation

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./vendor/robsontenorio/mary/src/View/Components/**/*.php",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require('daisyui'),
+    ],
+    daisyui: {
+        themes: ["light", "dark"],
+    },
+}
